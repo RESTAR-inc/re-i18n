fa534149a235dca92c5b875684b5837435a5dafc
Eugene Cheltsov <chill.icp@gmail.com>
2023-12-25 13:48:05 +0900

    fix: update vendor vue

--- lib/vendor/vue/index.d.ts
+++ lib/vendor/vue/index.d.ts
@@ -1,22 +1,3 @@
-import { type PropType } from "vue";
-import type { I18nFormatter, I18nLocaleKeyset, I18nParams } from "../../types";
-import type { UseLocaleLocator } from "./types";
-export declare function createI18n<L extends string, K extends string>(localeKeyset: I18nLocaleKeyset<L, K>, formatter: I18nFormatter<L>, useLocaleLocator: UseLocaleLocator<L>, defaultLocale: L): {
-    Component: import("vue").DefineComponent<{
-        msg: {
-            type: PropType<K>;
-            required: true;
-        };
-    }, (() => (string | import("vue").VNode<import("vue").RendererNode, import("vue").RendererElement, {
-        [key: string]: any;
-    }>)[]) | null, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
-        msg: {
-            type: PropType<K>;
-            required: true;
-        };
-    }>>, {}, {}>;
-    useI18n: () => (key: K, params?: I18nParams) => string;
-    locale: import("vue").WritableComputedRef<L>;
-};
-export * from "./types";
+export { createI18n } from "./createI18n.js";
+export * from "./types.js";
 //# sourceMappingURL=index.d.ts.map
