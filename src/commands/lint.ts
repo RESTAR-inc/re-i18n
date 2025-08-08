e676b7d612314768b490aadea5c629e16c763cd9
Nas Villanueva <jonas.villanueva@restar-inc.com>
2025-08-08 11:50:16 +0900

    Make lint command check for unsorted keys

--- src/commands/lint.ts
+++ src/commands/lint.ts
@@ -1,4 +1,5 @@
 import chalk from "chalk";
+import { isEqual, sortBy } from "lodash-es";
 import path from "path";
 import { parse } from "../parser/parse.js";
 import type { I18nConfig } from "../schemas/config.js";
@@ -6,23 +7,40 @@ import type { I18nConfig } from "../schemas/config.js";
 export async function lint(config: I18nConfig) {
   const data = parse({ config });
 
-  const keys: Array<{ path: string; key: string; reason: string }> = [];
+  const errors: Array<{ path: string; key?: string; reason: string }> = [];
 
   for (const [dir, rawData] of Object.entries(data)) {
+    const dataPath = path.resolve(dir);
+
     for (const key of rawData.stats.added) {
-      keys.push({ key, path: path.resolve(dir), reason: "A new key was found" });
+      errors.push({ key, path: dataPath, reason: "A new key was found" });
     }
     for (const key of rawData.stats.unused) {
-      keys.push({ key, path: path.resolve(dir), reason: "An unused key was found" });
+      errors.push({ key, path: dataPath, reason: "An unused key was found" });
+    }
+
+    const hasOutdatedTranslations = config.locales.some(
+      (locale) =>
+        !isEqual(
+          Object.keys(rawData.existingTranslations[locale]),
+          sortBy(Object.keys(rawData.existingTranslations[locale]))
+        )
+    );
+
+    if (hasOutdatedTranslations) {
+      errors.push({ path: dataPath, reason: "Unsorted locale files found." });
     }
   }
 
-  if (keys.length > 0) {
-    for (const key of keys) {
-      console.log(chalk.underline(key.path));
-      console.log(
-        `  ${chalk.red("error")}  ${key.reason}: "${chalk.bold(chalk.yellow(key.key))}"\n`
-      );
+  if (errors.length > 0) {
+    for (const error of errors) {
+      console.log(chalk.underline(error.path));
+
+      const message =
+        error.key == null
+          ? error.reason
+          : `${error.reason}: "${chalk.bold(chalk.yellow(error.key))}"`;
+      console.log(`  ${chalk.red("error")}  ${message}\n`);
     }
     throw new Error("Lint failed");
   }
