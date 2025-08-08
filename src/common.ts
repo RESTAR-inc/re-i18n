61ad4c21131c377111cc5c3f8703128d77a2fa28
Nas Villanueva <jonas.villanueva@restar-inc.com>
2025-08-08 11:51:52 +0900

    Use sortBy to sort keyset keys
    Since we use lodash sortBy to check for unsorted keys, we probably
    should use the same sort functions

--- src/common.ts
+++ src/common.ts
@@ -1,8 +1,8 @@
+import { sortBy } from "lodash-es";
 import type { I18nKeyset } from "./types.js";
 
 export function sortKeyset(target: I18nKeyset<string>): I18nKeyset<string> {
-  const keys = Object.keys(target);
-  keys.sort();
+  const keys = sortBy(Object.keys(target));
 
   const result: I18nKeyset<string> = {};
 
